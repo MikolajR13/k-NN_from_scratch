8c94e5a9875c1b8b1754cba9e4efe85acef180a5
MikolajR13 <piotrek.trebeusz@gmail.com>
2025-03-05 20:42:02 +0100

    First commit, created all files

---
+++ knn_classifier.py
@@ -0,0 +1,12 @@
+
+
+
+class KNNClassifier:
+    def __init__(self, k=3, metric='euclidean'):
+        pass
+
+    def fit(self, X_train, y_train):
+        pass
+
+    def predict(self, X_test):
+        pass
